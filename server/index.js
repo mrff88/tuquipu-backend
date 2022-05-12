7caeef85f97f1111040a7f4edf1b075f774e6581
mrff88 <mrff88@gmail.com>
2022-05-12 14:37:59 -0500

    build: add cors middleware to server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import requestID from 'express-request-id';
 import express from 'express';
+import cors from 'cors';
 import logger from './config/logger.js';
 import { routeNotFoundHandler, errorHandler } from './middlewares/index.js';
 
@@ -8,6 +9,13 @@ const app = express();
 // setup middlewares
 app.use(requestID());
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  })
+);
 
 app.use(logger.requests);
 
