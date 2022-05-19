0b8ce3e1ec84dfd64eca068f36dc9a071f4b409a
mrff88 <mrff88@gmail.com>
2022-05-19 12:58:34 -0500

    feat: add client endpoints to api

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import usersRoutes from './users/routes.js';
+import clientsRoutes from './clients/routes.js';
 
 const router = express.Router();
 
 router.use('/users', usersRoutes);
+router.use('/clients', clientsRoutes);
 
 export default router;
