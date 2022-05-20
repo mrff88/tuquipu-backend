ea51c80c146d695dc670c0bd99a6d116c279fdd4
mrff88 <mrff88@gmail.com>
2022-05-20 10:15:13 -0500

    feat: add devices endpoints to api

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import usersRoutes from './users/routes.js';
 import clientsRoutes from './clients/routes.js';
+import devicesRoutes from './devices/routes.js';
 
 const router = express.Router();
 
 router.use('/users', usersRoutes);
 router.use('/clients', clientsRoutes);
+router.use('/devices', devicesRoutes);
 
 export default router;
