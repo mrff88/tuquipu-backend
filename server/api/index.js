74d8dd7e7f7b92de3c5c561d2af69528a6d2306a
mrff88 <mrff88@gmail.com>
2022-05-20 16:48:45 -0500

    feat: add services endpoints to api

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import usersRoutes from './users/routes.js';
 import clientsRoutes from './clients/routes.js';
 import devicesRoutes from './devices/routes.js';
+import servicesRoutes from './services/routes.js';
 
 const router = express.Router();
 
 router.use('/users', usersRoutes);
 router.use('/clients', clientsRoutes);
 router.use('/devices', devicesRoutes);
+router.use('/services', servicesRoutes);
 
 export default router;
