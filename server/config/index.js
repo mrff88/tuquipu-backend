d55328a9e078ae31f7a4ff19641e12f25ded2272
mrff88 <mrff88@gmail.com>
2022-05-22 19:12:52 -0500

    refactor: fix heroku port error, sixth try

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 const config = {
   server: {
-    port: process.env.SERVER_PORT,
+    port: process.env.PORT,
   },
   database: {
     protocol: process.env.DATABASE_PROTOCOL,
